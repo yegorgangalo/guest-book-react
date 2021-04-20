7b6ce4d876c3dd446e2c7a953b1e5527bf8b65f9
Yegor Gangalo <gangaloyegor@gmail.com>
2021-04-20 13:24:49 +0300

    fix

--- src/state/API.js
+++ src/state/API.js
@@ -23,7 +23,9 @@ export async function deleteCommentAPI(_id) {
 
 //cache reducers
 export const commentsDataAdd = result => oldData => oldData.concat(result);
+
 export const commentsDataUpdate = result => oldData =>
   oldData.map(item => (item._id === result._id ? result : item));
+
 export const commentsDataRemove = result => oldData =>
   oldData.filter(({ _id }) => _id !== result);
