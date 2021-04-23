886ebde11569b31010ef18299bae20800582200c
Yegor Gangalo <gangaloyegor@gmail.com>
2021-04-23 13:12:18 +0300

    change server to heroku from localhost

--- src/redux/comments/comments-operations.js
+++ src/redux/comments/comments-operations.js
@@ -1,53 +1,56 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://guest-book-server.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:3003';
+axios.defaults.baseURL = 'https://guest-book-server.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:3003';
 
 export const fetchComments = createAsyncThunk(
-    'comments/fetch',
+  'comments/fetch',
   async (_, { rejectWithValue }) => {
     try {
-      const {data} = await axios.get('/api/comments');
+      const { data } = await axios.get('/api/comments');
       return data;
     } catch (error) {
-      return rejectWithValue(error.response)
+      return rejectWithValue(error.response);
     }
-  }
-)
+  },
+);
 
 export const addComment = createAsyncThunk(
-    'comments/add',
+  'comments/add',
   async (comment, { rejectWithValue }) => {
     try {
-      const {data} = await axios.post('/api/comments', comment);
-        return data;
+      const { data } = await axios.post('/api/comments', comment);
+      return data;
     } catch (error) {
-      return rejectWithValue(error.response)
+      return rejectWithValue(error.response);
     }
-  }
-)
+  },
+);
 
 export const deleteComment = createAsyncThunk(
-    'comments/delete',
+  'comments/delete',
   async (id, { rejectWithValue }) => {
     try {
       await axios.delete(`/api/comments/${id}`);
       return id;
     } catch (error) {
-      return rejectWithValue(error.response)
+      return rejectWithValue(error.response);
     }
-  }
-)
+  },
+);
 
 export const patchComment = createAsyncThunk(
-    'comments/patch',
-  async ({_id, name, comment}, { rejectWithValue }) => {
+  'comments/patch',
+  async ({ _id, name, comment }, { rejectWithValue }) => {
     try {
-      const {data} = await axios.patch(`/api/comments/${_id}`, { name, comment });
+      const { data } = await axios.patch(`/api/comments/${_id}`, {
+        name,
+        comment,
+      });
       return data;
     } catch (error) {
-      return rejectWithValue(error.response)
+      return rejectWithValue(error.response);
     }
-  }
-)
+  },
+);
