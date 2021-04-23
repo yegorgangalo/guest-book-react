4447dccf8d68528d6f5c004573298eab1d7959bf
Yegor Gangalo <gangaloyegor@gmail.com>
2021-04-23 14:12:53 +0300

    made refactoring

--- src/cache/react-query.js
+++ src/cache/react-query.js
@@ -9,37 +9,64 @@ export const cache = {
 };
 
 //cache handlers
-const commentAddToCache = result => oldData => oldData.concat(result);
-const commentUpdateInCache = result => oldData =>
-  oldData.map(item => (item._id === result._id ? result : item));
-const commentDeleteFromCache = result => oldData =>
-  oldData.filter(({ _id }) => _id !== result);
+const commentAddToCache = result => oldData => {
+  return oldData.concat(result);
+};
+const commentUpdateInCache = result => oldData => {
+  return oldData.map(item => (item._id === result._id ? result : item));
+};
+const commentDeleteFromCache = result => oldData => {
+  return oldData.filter(({ _id }) => _id !== result);
+};
 
 //hooks
-export const useGetAllComments = () => {
-  return useQuery(cache.allComments, API.getAllComments);
-};
-
-export const useDeleteComment = () => {
+const mutationQueryTemplate = (
+  handleAPI,
+  onSuccessHandleCache,
+  typeCache = cache.allComments,
+) => () => {
   const queryClient = useQueryClient();
-  return useMutation(API.deleteComment, {
+  return useMutation(handleAPI, {
     onSuccess: data =>
-      queryClient.setQueryData(cache.allComments, commentDeleteFromCache(data)),
+      queryClient.setQueryData(typeCache, onSuccessHandleCache(data)),
   });
 };
 
-export const usePostComment = () => {
-  const queryClient = useQueryClient();
-  return useMutation(API.postComment, {
-    onSuccess: data =>
-      queryClient.setQueryData(cache.allComments, commentAddToCache(data)),
-  });
-};
+export const useDeleteComment = mutationQueryTemplate(
+  API.deleteComment,
+  commentDeleteFromCache,
+);
+export const usePostComment = mutationQueryTemplate(
+  API.postComment,
+  commentAddToCache,
+);
+export const usePatchComment = mutationQueryTemplate(
+  API.patchComment,
+  commentUpdateInCache,
+);
+export const useGetAllComments = () =>
+  useQuery(cache.allComments, API.getAllComments);
 
-export const usePatchComment = () => {
-  const queryClient = useQueryClient();
-  return useMutation(API.patchComment, {
-    onSuccess: data =>
-      queryClient.setQueryData(cache.allComments, commentUpdateInCache(data)),
-  });
-};
+// export const useDeleteComment = () => {
+//   const queryClient = useQueryClient();
+//   return useMutation(API.deleteComment, {
+//     onSuccess: data =>
+//       queryClient.setQueryData(cache.allComments, commentDeleteFromCache(data)),
+//   });
+// };
+
+// export const usePostComment = () => {
+//   const queryClient = useQueryClient();
+//   return useMutation(API.postComment, {
+//     onSuccess: data =>
+//       queryClient.setQueryData(cache.allComments, commentAddToCache(data)),
+//   });
+// };
+
+// export const usePatchComment = () => {
+//   const queryClient = useQueryClient();
+//   return useMutation(API.patchComment, {
+//     onSuccess: data =>
+//       queryClient.setQueryData(cache.allComments, commentUpdateInCache(data)),
+//   });
+// };
